7dd6be31791c6a874ad93cc8d779c7324699e6cf
Lex Mel <lex07672@gmail.com>
2024-01-05 20:58:59 +0200

    complete 7

--- src/conditions-n-loops-tasks.js
+++ src/conditions-n-loops-tasks.js
@@ -222,8 +222,14 @@ function convertNumberToString(numberStr) {
  *  '0123210'   => true
  *  'qweqwe'    => false
  */
-function isPalindrome(/* str */) {
-  throw new Error('Not implemented');
+function isPalindrome(str) {
+  let reversedWord = '';
+
+  for (let i = str.length - 1; i >= 0; i -= 1) {
+    reversedWord += str[i];
+  }
+
+  return str === reversedWord;
 }
 
 /**
